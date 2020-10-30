383454d5c75ac66a04e122d2631a22c111d6709d
XabierTrabajo <73170218+XabierTrabajo@users.noreply.github.com>
2020-10-30 12:43:58 +0100

    bat v1.1

--- Administrar usuarios.bat
+++ Administrar usuarios.bat
@@ -1,11 +1,13 @@
 @echo off
 set choice=
 
+Rem Menu Principal
 :start
 echo Bienvenido, que deseas hacer?
 pause
 :Seleccion1
 cls
+echo Menu principal
 echo=================================
 echo Selecciona una opcion:
 echo (1) Opciones de usuarios.
@@ -20,10 +22,12 @@ if '%choice%' == '1' goto OpcionesU
 if '%choice%' == '2' goto OpcionesG
 if '%choice%' == '0' goto Salir
 
-echo El numero "%choice%" no es valido.
+Tu seleccion ("%choice%") no es valida
 pause
 goto Seleccion1
+Rem Fin Menu Principal
 
+Rem Menu de Usuarios
 :OpcionesU
 cls
 echo Has seleccionado las opciones de usuarios, que deseas hacer?
@@ -48,10 +52,12 @@ if '%choice%' == '3' goto AgregarUG
 if '%choice%' == '4' goto EliminarUG
 if '%choice%' == '0' goto Seleccion1
 
-echo El numero "%choice%" no es valido
+Tu seleccion ("%choice%") no es valida
 pause
 goto Seleccion2
+Rem Fin Menu de Usuarios
 
+Rem Menu de grupos
 :OpcionesG
 cls
 echo Has seleccionado las Opciones de grupo, que deseas hacer?
@@ -59,6 +65,8 @@ pause
 :Seleccion3
 cls
 
+
+echo Menu de grupos
 echo=================================
 echo Selecciona una opcion:
 echo (1) Agregar un grupo
@@ -75,15 +83,103 @@ if '%choice%' == '3' goto AgregarUG
 if '%choice%' == '4' goto EliminarUG
 if '%choice%' == '0' goto Seleccion1
 
-echo El numero "%choice%" no es valido
+echo Tu seleccion ("%choice%") no es valida
 pause
 goto Seleccion3
+Rem Fin Menu de grupos
+
+Rem Agregar usuario
+:AgregarU
+cls
+echo Introduce el usuario que quieras crear
+
+set /p usuario=
+:PreguntaU
+cls
+echo Se va a crear el usuario "%usuario%", quieres continuar? (s=Si/n=No)
+set /p choice=
+if '%choice%' == 'S'    goto    AgregandoU 
+if '%choice%' == 's'    goto    AgregandoU 
+if '%choice%' == 'Si'    goto    AgregandoU 
+if '%choice%' == 'si'    goto    AgregandoU 
+if '%choice%' == 'n'    goto Seleccion2
+if '%choice%' == 'no'    goto Seleccion2
+if '%choice%' == 'N'    goto Seleccion2
+if '%choice%' == 'No'    goto Seleccion2
+
+echo Tu seleccion ("%choice%") no es valida
+pause  
+goto :PreguntaU
+
+:AgregandoU
+echo Creando usuario "%usuario%"...
+echo=================================
 
 
+REM codigo
 
+
+echo=================================
+echo Se ha creado el usuario "%usuario%" correctamente.
+pause
+:preguntaU2
+cls
+echo Quieres crear otro usuario? (s=Si)/(n=No)
+set /p choice=
+if '%choice%' == 'S'    goto    AgregarU 
+if '%choice%' == 's'    goto    AgregarU 
+if '%choice%' == 'Si'    goto    AgregarU 
+if '%choice%' == 'si'    goto    AgregarU
+if '%choice%' == 'n'    goto Seleccion1
+if '%choice%' == 'no'    goto Seleccion1
+if '%choice%' == 'N'    goto Seleccion1
+if '%choice%' == 'No'   goto Seleccion1
+echo Tu seleccion ("%choice%") no es valida
+pause  
+goto :PreguntaU2
+
+Rem FIN Agregar usuario
+
+Rem Agregar Grupo
+:AgregarG
+cls
+echo Introduce el grupo que quieras crear
+
+set /p grupo=
+cls
+
+echo Se va a crear el grupo "%grupo%", quieres continuar? (S/N)
+set /p choice=
+if '%choice%' == 'S'    goto    AgregandoG 
+if '%choice%' == 's'    goto    AgregandoG 
+if '%choice%' == 'Si'    goto    AgregandoG 
+if '%choice%' == 'si'    goto    AgregandoG 
+goto AgregarG
+
+:AgregandoG
+echo Creando grupo "%grupo%"...
+echo=================================
+
+
+REM codigo
+
+
+echo=================================
+echo Se ha creado el grupo "%grupo%" correctamente.
+pause
+cls
+echo Quieres crear otro grupo? (S/N)
+set /p choice=
+if '%choice%' == 'S'    goto    AgregarG 
+if '%choice%' == 's'    goto    AgregarG 
+if '%choice%' == 'Si'    goto    AgregarG 
+if '%choice%' == 'si'    goto    AgregarG
+goto Seleccion1
+Rem FIN Agregar grupo
 
 
 :Salir
 cls
 echo Agur.
+pause 
 exit
